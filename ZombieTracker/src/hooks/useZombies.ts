121ec0758b31059b63e5f0a9c7c551af761609b8
Flavio Perez Ondarts <perezflavio94@gmail.com>
2021-02-14 20:11:42 -0300

    Updated useZombies hook

--- ZombieTracker/src/hooks/useZombies.ts
+++ ZombieTracker/src/hooks/useZombies.ts
@@ -15,9 +15,24 @@ const useZombies = () => {
             .finally(() => setLoading(false))
     }
 
+    const updateZombies = (updatedZombies: Zombie[]) => {
+        const notUpdated = zombies.filter(({ id }) => !updatedZombies.find(updated => updated.id === id))
+        setZombies([...notUpdated, ...updatedZombies])
+    }
+
+    const transferZombies = (ids: string[], destination: string) => {
+        setLoading(true)
+        axios.patch('http://localhost:3000/zombies/', {
+            zombies: ids.map(id => ({ id, location: destination }))
+        })
+        .then(res => updateZombies(res.data.data))
+        .catch(err => console.log(err))
+        .finally(() => setLoading(false))
+    }
+
     useEffect(() => fetchZombies(), [])
 
-    return { zombies, loading, fetchZombies }
+    return { zombies, loading, fetchZombies, transferZombies }
 }
 
 export default useZombies
