54e85f1cc9bcf882ccee4b56c4dffc54132d6605
Flavio Perez Ondarts <perezflavio94@gmail.com>
2021-02-14 19:56:16 -0300

    Created Menu component

---
+++ ZombieTracker/src/components/Menu.tsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { colors } from '../theme'
+import { MenuOption } from '../types'
+import ModalMenu from './ModalMenu'
+
+const Menu = ({
+    value,
+    onSelectOption,
+    options,
+    placeholder
+}: LocationsMenuProps) => {
+
+    const [visible, setVisible] = useState(false)
+    const label = options.find(option => option.value === value)?.label || placeholder
+
+    useEffect(() => {
+        if (visible) {
+            setVisible(false)
+        }
+    }, [value])
+
+    return (
+        <View>
+            <TouchableOpacity
+                onPress={() => setVisible(true)}
+                style={styles.button}
+            >
+                <Text style={styles.label}>
+                    {label}
+                </Text>
+            </TouchableOpacity>
+            <ModalMenu
+                visible={visible}
+                options={options}
+                onDismiss={() => setVisible(false)}
+                value={value}
+                onSelectOption={onSelectOption}
+            />
+        </View>
+    )
+}
+
+interface LocationsMenuProps {
+    options: MenuOption[]
+    value?: string
+    onSelectOption: (option: string) => void
+    placeholder: string
+}
+
+const styles = StyleSheet.create({
+    button: {
+        width: 128,
+        borderWidth: 1,
+        borderRadius: 16,
+        margin: 8,
+        marginBottom: 16,
+        borderColor: colors.darkGray,
+        backgroundColor: colors.button,
+        shadowRadius: 2,
+        shadowOpacity: 0.5,
+        shadowOffset: {
+            height: 2,
+            width: 2
+        },
+        elevation: 2
+    },
+    label: {
+        marginLeft: 4,
+        fontSize: 16,
+        padding: 4,
+        color: colors.white
+    }
+})
+export default Menu
