b3ffa919f0dc6cff1d22e49d6d94e3379ce12a09
Flavio Perez Ondarts <perezflavio94@gmail.com>
2021-02-14 20:10:32 -0300

    Added patch method to /zombies endpoint

--- backend/routes/zombies.js
+++ backend/routes/zombies.js
@@ -25,6 +25,30 @@ router.get('/', function(req, res, next) {
     })
 });
 
+router.patch('/', function(req, res) {
+    zombies.forEach(zombie => { 
+        const updated_zombie = req.body.zombies.find(({ id }) => zombie.id === id) || {}
+        if (updated_zombie.location) {
+            zombie.location = updated_zombie.location
+        }
+        if (updated_zombie.name) {
+            zombie.name = updated_zombie.name
+        }
+    })
+    res.status(200).json({
+        data: zombies
+    })
+});
+
+router.get('/:id', function(req, res) {
+    const zombie = zombies.find(({ id }) => id === req.params.id)
+    if (zombie) {
+        res.status(200).json({
+            data: zombie
+        })
+    }
+})
+
 router.post('/', function(req, res) {
     const zombie = { id: uuidv4(), name: req.body.name, location: req.body.location }
     zombies.push(zombie)
