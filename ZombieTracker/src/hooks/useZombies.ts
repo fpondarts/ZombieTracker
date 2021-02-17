fe6cf7fd813e279329ced5458e03c3031c677e09
Flavio Perez Ondarts <perezflavio94@gmail.com>
2021-02-17 01:34:59 -0300

    Remove unnecesary array processing

--- ZombieTracker/src/hooks/useZombies.ts
+++ ZombieTracker/src/hooks/useZombies.ts
@@ -15,17 +15,12 @@ const useZombies = () => {
             .finally(() => setLoading(false))
     }
 
-    const updateZombies = (updatedZombies: Zombie[]) => {
-        const notUpdated = zombies.filter(({ id }) => !updatedZombies.find(updated => updated.id === id))
-        setZombies([...notUpdated, ...updatedZombies])
-    }
-
     const transferZombies = (ids: string[], destination: string) => {
         setLoading(true)
         axios.patch('http://localhost:3000/zombies/', {
             zombies: ids.map(id => ({ id, location: destination }))
         })
-        .then(res => updateZombies(res.data.data))
+        .then(res => setZombies(res.data.data))
         .catch(err => console.log(err))
         .finally(() => setLoading(false))
     }
